dac97cf9b24a72c2014edb057b61b11c63889382
Marcio José de oliveira <marciojo77rj@hotmail.com>
2025-04-22 11:27:38 -0300

    modified:

--- config.php
+++ config.php
@@ -22,7 +22,7 @@ ini_set('session.use_strict_mode', 1);
 ini_set('session.cookie_samesite', 'Strict');
 ini_set('session.gc_maxlifetime', 1800); // 30 minutos
 **/
-<?php
+
 if (session_status() === PHP_SESSION_NONE) {
     ini_set('session.cookie_httponly', 1);
     ini_set('session.cookie_secure', 1);
