de260e1b7568caf407836dc7ecdca91cd75139ad
Marcio José de oliveira <marciojo77rj@hotmail.com>
2025-04-28 11:18:52 -0300

    index.php

--- public/index.php
+++ public/index.php
@@ -3,8 +3,8 @@
  * INDEX.PHP - Pagina inicial do sistema
  */
 session_start();
-require_once 'src/config.php';
-require_once 'utils/functions.php';
+require_once 'src/config/config.php';
+require_once 'src/utils/functions.php';
 
 // Verificar se o usuario esta logado
 if (!isLoggedIn()) {
